e655915e5dc17f494b67742bedadea74cbb4523e
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-02-25 15:20:47 +0300

    add this function

--- static/main.js
+++ static/main.js
@@ -1,7 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Get the form element
-var form = document.getElementById("myForm");
+    const correctAnswers = JSON.parse(document.getElementById('correct-answers').textContent);
 
-// Add event listener to the form for submit event
-form.addEventListener("submit", function(event) {
+    function checkAnswer(index) {
+        const inputField = document.getElementById(`answer-${index}`);
+        const userAnswer = inputField.value.trim().toLowerCase();
+        const correctAnswer = correctAnswers[index].trim().toLowerCase();
+
+        if (userAnswer === correctAnswer) {
+            inputField.classList.remove("incorrect");
+            inputField.classList.add("correct");
+            // Отправка статистики на сервер
+            fetch(`/update_score`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ correct: true })
+            });
+        } else {
+            inputField.classList.remove("correct");
+            inputField.classList.add("incorrect");
+        }
+    }
+
+    // Привязка обработчиков событий к кнопкам
+    const buttons = document.querySelectorAll('.submit-button');
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => checkAnswer(index));
+    });
 });
