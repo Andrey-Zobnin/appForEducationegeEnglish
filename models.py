49cc891a9cbca467f99e5ec03d4ba74643e631ac
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-02-25 10:21:21 +0300

    Added user authentication and score tracking features`

---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    correct_answers = db.Column(db.Integer, default=0)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(500), nullable=False)
+    correct_answer = db.Column(db.String(150), nullable=False)
