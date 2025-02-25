d4f3493b3e6af7d64d249971fe21c8c7c4e1d9ef
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-02-25 15:19:55 +0300

    add main.js with error and with pattern to addEventListner

---
+++ static/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the form element
+var form = document.getElementById("myForm");
+
+// Add event listener to the form for submit event
+form.addEventListener("submit", function(event) {
+});
